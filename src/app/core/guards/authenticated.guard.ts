8ff27dda1415e8aab03786e202bafae9e79d3cd0
RamezTaher <ramezgamer11@gmail.com>
2025-01-10 15:31:15 +0100

    fix guards

--- src/app/core/guards/authenticated.guard.ts
+++ src/app/core/guards/authenticated.guard.ts
@@ -4,13 +4,13 @@ import { CanActivate, Router } from '@angular/router'
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthenticatedGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
     const accessToken = localStorage.getItem('token')
-    if (accessToken) {
-      this.router.navigate(['/'])
+    if (!accessToken) {
+      this.router.navigate(['/pages-login-2'])
       return false
     }
     return true
