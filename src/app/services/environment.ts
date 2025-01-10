c7a97eee651161ce1b3dd4df0e768ff00e6bc339
RamezTaher <ramezgamer11@gmail.com>
2025-01-10 15:30:23 +0100

    update env

--- src/app/services/environment.ts
+++ src/app/services/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
-    production: false,
-    alphaVantageApiKey: 'UNRHD8QFWSD75Q5E', // Replace with your API key
-    //alphaVantageApiKey: 'QEPFED6K9UNZCMCW', // Replace with your API key
-    //alphaVantageApiKey: 'demo', // Replace with your API key
-    baseUrl : 'https://www.alphavantage.co/query'
-  };
-  
+  production: false,
+  alphaVantageApiKey: 'UNRHD8QFWSD75Q5E', // Replace with your API key
+  //alphaVantageApiKey: 'QEPFED6K9UNZCMCW', // Replace with your API key
+  //alphaVantageApiKey: 'demo', // Replace with your API key
+  baseUrl: 'http://localhost:5000/api',
+}
