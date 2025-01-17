cec60376dff4725f98f836aa61873694bcab7ef8
RamezTaher <ramezgamer11@gmail.com>
2025-01-17 19:10:31 +0100

    create route and component for the chat messages

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,46 +1,110 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { User } from 'stream-chat';
-import { ChatClientService, ChannelService, StreamI18nService, StreamAutocompleteTextareaModule,StreamChatModule } from 'stream-chat-angular';
+import { GroupChatService } from '@/app/services/group-chat.service'
+import { SocketService } from '@/app/services/socket.service'
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { ActivatedRoute } from '@angular/router'
+import { TranslateModule } from '@ngx-translate/core'
+import { Subscription } from 'rxjs'
+import { User } from 'stream-chat'
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [TranslateModule, StreamAutocompleteTextareaModule, StreamChatModule],
+  imports: [TranslateModule, FormsModule, CommonModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
 export class ChatComponent implements OnInit {
+  groupId!: string
+  user!: { _id: string; firstname: string; lastname: string }
+  messages: any[] = []
+  newMessage = ''
+  routeSubscription!: Subscription
+  socketSubscriptions: Subscription[] = []
+  loadingMessages = true
+
   constructor(
-    private chartService: ChatClientService,
-    private channelService: ChannelService,
-    private streamI18nService: StreamI18nService,
-  ) {
-    const apiKey = 'dz5f4d5kzrue';
-    const userId = 'ancient-bonus-9';
-    const userToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZmxhdC13aW5kLTQiLCJleHAiOjE3MzYxMzE1OTF9.Vf3CVB-Dj5wKI0taUUbLV4u-JVFekE6QBwVTmOc3y94';
-    const userName = 'ancient';
+    private socketService: SocketService,
+    private route: ActivatedRoute,
+    private groupChatService: GroupChatService
+  ) {}
 
-    const user: User = {
-      id: userId,
-      name: userName,
-      image:`https://getstream.io/random_png/?name=${userName}`,
-    };
+  ngOnInit(): void {
+    // Parse the user object from local storage
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      try {
+        this.user = JSON.parse(userString)
+      } catch (error) {
+        console.error('Failed to parse user data:', error)
+        return
+      }
+    }
 
-    this.chartService.init(apiKey, user, userToken);
-    this.streamI18nService.setTranslation();
+    this.routeSubscription = this.route.paramMap.subscribe((paramMap) => {
+      this.groupId = paramMap.get('id') || ''
+
+      if (this.groupId && this.user?._id) {
+        this.loadingMessages = true
+        this.initializeChat(this.groupId)
+      } else {
+        console.error('Invalid group ID or user data')
+      }
+    })
   }
 
-  async ngOnInit() {
-    const channel = this.chartService.chatClient.channel('messaging', 'Financial-Market',{
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
-      name:'Financial Market',
-    });
+  private initializeChat(groupId: string): void {
+    this.loadOldMessages(groupId)
+    this.socketService.joinGroup(groupId)
 
-    await channel.create();
-    this.channelService.init({
-      type:'messaging',
-      id:{$eq: 'Financial-Market'},
+    this.socketSubscriptions.forEach((sub) => sub.unsubscribe())
+    this.socketSubscriptions = []
+
+    // Listen for new messages
+    const newMessageSub = this.socketService
+      .onNewMessage()
+      .subscribe((message) => {
+        this.messages.push(message)
+      })
+    this.socketSubscriptions.push(newMessageSub)
+
+    const errorSub = this.socketService.onError().subscribe((error) => {
+      console.error('Error:', error)
     })
+    this.socketSubscriptions.push(errorSub)
+  }
+
+  private loadOldMessages(groupId: string): void {
+    this.groupChatService.getGroupChatById(groupId).subscribe(
+      (groupChat) => {
+        this.messages = groupChat.messages || []
+        this.loadingMessages = false
+      },
+      (error) => {
+        console.error('Error loading old messages:', error)
+        this.loadingMessages = false
+      }
+    )
+  }
+
+  sendMessage(): void {
+    if (this.newMessage.trim() && this.groupId && this.user?._id) {
+      this.socketService.sendMessage(
+        this.groupId,
+        this.user._id, // Use the user's `_id` from the parsed object
+        this.newMessage
+      )
+      this.newMessage = ''
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe()
+    }
+    this.socketSubscriptions.forEach((sub) => sub.unsubscribe())
+    this.socketService.disconnect()
   }
 }
