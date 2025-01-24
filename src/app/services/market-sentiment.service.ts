f88a81bd95e474b125baa60277a134383953d02a
ddevsidas <dharmindar81@outlook.com>
2025-01-24 14:56:57 -0500

    sentiment url update

--- src/app/services/market-sentiment.service.ts
+++ src/app/services/market-sentiment.service.ts
@@ -12,7 +12,8 @@ export class MarketSentimentService {
 
   // Fetch news sentiment
   getNewsSentiment(): Observable<any> {
-    const url = `${environment.sentimentUrl}`;
+    //const url = `${environment.sentimentUrl}`;
+    const url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=UNRHD8QFWSD75Q5E';
     return this.http.get(url);
   }
 }
